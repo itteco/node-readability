212461cc40c11614864061fad100c28740f3c550
joiningss <694619787@qq.com>
2014-04-24 17:40:00 +0800

    commit some change for preview generator

--- test/preview-generator.js
+++ test/preview-generator.js
@@ -1,3 +1,23 @@
 /**
  * Created by joiningss on 4/23/14.
  */
+
+var util = require('util');
+var request = require('request');
+var _ = require('underScore');
+var readability2 = require('../lib/readability.js');
+
+// If enableHtmlCache is true, generator will use name-matching html files in ./preview-html-cache before download it
+var enableHtmlCache = false;
+// urls for parse
+var targetHtmlURLs = ['',
+                      ''];
+
+
+var startGenerator = function(){
+  targetHtmlURLs = _.unique(targetHtmlURLs);
+
+
+
+}
+
